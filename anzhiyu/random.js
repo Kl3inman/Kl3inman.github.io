4427cb38ee18e75d5c37df1b7e9e2444d9d9f440
kl3inman <1315720885@qq.com>
2025-02-14 18:42:48 +0800

    Site updated: 2025-02-14 18:42:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/24/hello-world/"];function toRandomPost(){
+var posts=["2025/01/24/hello-world/","2025/02/14/大二上学期回顾与计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
