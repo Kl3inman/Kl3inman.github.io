e1a9e5630ca09b35c2cb84e0511fe10aa11981ed
kl3inman <1315720885@qq.com>
2025-02-21 18:50:51 +0800

    Site updated: 2025-02-21 18:50:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/21/hello-world/","2025/02/14/Essence of linear algebra/","2025/02/14/大二上学期回顾与计划/"];function toRandomPost(){
+var posts=["2025/02/14/Essence of linear algebra/","2025/02/21/hello-world/","2025/02/14/大二上学期回顾与计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
